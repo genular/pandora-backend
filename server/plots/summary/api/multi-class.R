cee6a25867de0180ff27ea573ad2a4e663f62cab
LogIN- <info@ivantomic.com>
2024-04-11 18:18:20 +0100

    [hotfix] tsne plots

--- server/plots/summary/api/multi-class.R
+++ server/plots/summary/api/multi-class.R
@@ -52,11 +52,13 @@ pandora$handle$plots$modelsummary$renderPlot$multiClass <- expression(
 
         plot_unique_hash <- list(
             training = list(
-                auc_roc = list()
+                auc_roc = list(),
+                auc_roc_multiclass = list()
             ),
             ## ROC for Testing predictions on out leave out data
             testing = list(
-                auc_roc = list()
+                auc_roc = list(),
+                auc_roc_multiclass = list()
             ),
             saveObjectHash = digest::digest(paste0(resampleID, "_",args$settings,"_exploration_modelsummary"), algo="md5", serialize=F)
         )
@@ -82,8 +84,12 @@ pandora$handle$plots$modelsummary$renderPlot$multiClass <- expression(
                 if(modelDataExists) {
 
                     class_mapping <- setNames(modelData$info$outcome_mapping$class_original, modelData$info$outcome_mapping$class_remapped)
+                    observed <- modelData$training$raw$data$pred$obs
+                    classes <- unique(observed)
 
-                    ## TESTING ROC:
+                    ## TESTING ROC (PLOT 1):
+                    ## Extended One-vs-All Comparisons | MULTI
+
                     roc_objs <- modelData$predictions$AUROC$Multiclass$ROC$rocs
                     auc_value <- NA
 
@@ -112,19 +118,67 @@ pandora$handle$plots$modelsummary$renderPlot$multiClass <- expression(
                     roc_data <- Filter(Negate(is.null), roc_data)
                     # Combine all ROC data into a single data frame
                     roc_data_df <- do.call(rbind, roc_data)
-                    
+
                     # Update the Class column in roc_data_df
                     roc_data_df$Class <- sapply(roc_data_df$Class, update_class_labels, class_mapping)
 
-                    plot_unique_hash[["testing"]]$auc_roc[[method]] <- digest::digest(paste0(resampleID, "_",args$settings,"_testing_auc_", method), algo="md5", serialize=F)
-                    tmp_path <- plot_auc_roc_testing_multiclass(roc_data_df, auc_value, settings, plot_unique_hash[["testing"]]$auc_roc[[method]])
+                    plot_unique_hash[["testing"]]$auc_roc_multiclass[[method]] <- digest::digest(paste0(resampleID, "_",args$settings,"_testing_auc_roc_multiclass_", method), algo="md5", serialize=F)
+                    tmp_path <- plot_auc_roc_multiclass_testing(roc_data_df, auc_value, settings, plot_unique_hash[["testing"]]$auc_roc_multiclass[[method]])
+
+                    res.data$testing$auc_roc_multiclass[[method]] <- optimizeSVGFile(tmp_path)
+                    res.data$testing$auc_roc_multiclass_png[[method]] <- convertSVGtoPNG(tmp_path)
+
+                    ## TESTING ROC (PLOT 2):
+                    ## One-vs-All Strategy | SINGLE
+                    
+                    # Initialize an empty dataframe to store ROC data
+                    roc_data_df <- data.frame(FPR = numeric(), TPR = numeric(), Thresholds = numeric(), Class = factor(), AUC = numeric())
+
+                    # Loop over each class in the AUROC list, assuming each class has its separate ROC data
+                    for(class_name in names(modelData$predictions$AUROC)) {
+                        if(class_name == "Multiclass") {
+                            next
+                        }
+                        # Access the ROC object and AUC value for each class
+                        roc_obj <- modelData$predictions$AUROC[[class_name]]$ROC
+                        auc_value <- as.numeric(modelData$predictions$AUROC[[class_name]]$AUC)
+                        
+                        # Check if the roc_obj is correctly structured and contains the data
+                        if(is.list(roc_obj) && "specificities" %in% names(roc_obj) && "sensitivities" %in% names(roc_obj)) {
+                            tmp_data <- data.frame(
+                                FPR = 1 - roc_obj$specificities,
+                                TPR = roc_obj$sensitivities,
+                                Thresholds = roc_obj$thresholds,
+                                Class = class_name,  # Use the class name directly
+                                AUC = rep(auc_value, length(roc_obj$specificities))  # Repeat AUC value for length of the specifics
+                            )
+                            # Bind this temporary data frame to the main ROC data frame
+                            roc_data_df <- rbind(roc_data_df, tmp_data)
+                        }
+                    }
+
+                    # Ensure Class and AUC columns are properly formatted
+                    roc_data_df$Class <- factor(roc_data_df$Class)
+                    roc_data_df$AUC <- as.numeric(roc_data_df$AUC)
+
+                    # Calculate the average AUC for each class
+                    average_auc_per_class <- aggregate(AUC ~ Class, data = roc_data_df, FUN = mean)
+
+                    # Create labels in the format "Class: AUC"
+                    auc_labels <- sprintf("%s - (%.2f)", average_auc_per_class$Class, average_auc_per_class$AUC)
+
+                    # Optionally, you can name the labels by their class for easier referencing in plots
+                    names(auc_labels) <- average_auc_per_class$Class
+
+                    plot_unique_hash[["testing"]]$auc_roc[[method]] <- digest::digest(paste0(resampleID, "_",args$settings,"_testing_auc_roc_", method), algo="md5", serialize=F)
+                    tmp_path <- plot_auc_roc_multiclass_testing_single(roc_data_df, auc_labels, settings, plot_unique_hash[["testing"]]$auc_roc[[method]])
 
                     res.data$testing$auc_roc[[method]] <- optimizeSVGFile(tmp_path)
                     res.data$testing$auc_roc_png[[method]] <- convertSVGtoPNG(tmp_path)
 
-                    ## TRAINIG ROC:
-                    observed <- modelData$training$raw$data$pred$obs
-                    classes <- unique(observed)
+
+                    ## TRAINIG ROC (PLOT 1):
+                    ## One-vs-All Strategy | SINGLE
                     roc_list <- list()
 
                     for (class in classes) {
@@ -160,13 +214,75 @@ pandora$handle$plots$modelsummary$renderPlot$multiClass <- expression(
 
                     roc_data$Class <- class_mapping[roc_data$Class]
                     names(auc_values) <- class_mapping[names(auc_values)]
-                    auc_labels <- sprintf("%s: %.2f", names(auc_values), auc_values)
+                    auc_labels <- sprintf("%s - (%.2f)", names(auc_values), auc_values)
 
-                    plot_unique_hash[["training"]]$auc_roc[[method]] <- digest::digest(paste0(resampleID, "_",args$settings,"_training_auc_", method), algo="md5", serialize=F)
-                    tmp_path <- plot_auc_roc_trainig_multiclass(roc_data, auc_labels, settings, plot_unique_hash[["training"]]$auc_roc[[method]])
+                    plot_unique_hash[["training"]]$auc_roc[[method]] <- digest::digest(paste0(resampleID, "_",args$settings,"_training_auc_roc_", method), algo="md5", serialize=F)
+                    tmp_path <- plot_auc_roc_multiclass_training_single(roc_data, auc_labels, settings, plot_unique_hash[["training"]]$auc_roc[[method]])
 
                     res.data$training$auc_roc[[method]] <- optimizeSVGFile(tmp_path)
                     res.data$training$auc_roc_png[[method]] <- convertSVGtoPNG(tmp_path)
+
+
+                    ## TRAINIG ROC (PLOT 2):
+                    ## Extended One-vs-All Comparisons | MULTI
+                    
+                    # Initialize an empty data frame for ROC data
+                    roc_data_df <- data.frame(FPR = numeric(), TPR = numeric(), Class = factor(), Comparison = factor(), Thresholds = numeric())
+                    auc_values <- list()  # Use a list to store AUC values for easier handling
+
+                    # Loop through each class and compute ROC against each other class
+                    for (primary_class in classes) {
+                        for (comparison_class in classes) {
+                            if (primary_class != comparison_class) {
+                                binary_observed <- ifelse(observed == primary_class, 1, 0)
+                                binary_predicted <- modelData$training$raw$data$pred[[comparison_class]]
+                                
+                                # Compute ROC curve using pROC
+                                roc_obj <- pROC::roc(binary_observed, binary_predicted)
+                                
+                                # Create a temporary data frame with ROC data
+                                tmp_data <- data.frame(
+                                    FPR = 1 - roc_obj$specificities,
+                                    TPR = roc_obj$sensitivities,
+                                    Thresholds = roc_obj$thresholds,
+                                    Class = class_mapping[primary_class],
+                                    Comparison = class_mapping[comparison_class]  # New field for comparison class
+                                )
+                                
+                                # Bind this temporary data frame to the main ROC data frame
+                                roc_data_df <- rbind(roc_data_df, tmp_data)
+                                
+                                # Store AUC value in the list with an appropriate label
+                                auc_values[[paste(class_mapping[primary_class], "vs", class_mapping[comparison_class])]] <- auc(roc_obj)
+                            }
+                        }
+                    }
+
+                    # Assuming 'Class' and 'Comparison' are already in the desired format but need pairing
+                    roc_data_df$Comparison <- with(roc_data_df, paste(Class, "vs", Comparison))
+
+                    # Now adjust the creation of AUC labels to match this new pairing format
+                    auc_labels <- sapply(names(auc_values), function(name) {
+                        sprintf("%s - (%.2f)", name, auc_values[[name]])
+                    })
+
+                    # Create a dataframe for AUC labels
+                    auc_labels_df <- data.frame(
+                        Label = auc_labels,
+                        ClassComparison = names(auc_values)
+                    )
+
+                    roc_data_df$Comparison <- as.character(roc_data_df$Comparison)
+                    auc_labels_df$ClassComparison <- as.character(auc_labels_df$ClassComparison)
+
+                    # Merge AUC labels into the roc_data
+                    roc_data_df <- base::merge(roc_data_df, auc_labels_df, by.x = "Comparison", by.y = "ClassComparison", all.x = TRUE)
+
+                    plot_unique_hash[["training"]]$auc_roc_multiclass[[method]] <- digest::digest(paste0(resampleID, "_",args$settings,"_training_auc_roc_multiclass_", method), algo="md5", serialize=F)
+                    tmp_path <- plot_auc_roc_multiclass_training(roc_data_df, auc_labels_df, settings, plot_unique_hash[["training"]]$auc_roc_multiclass[[method]])
+
+                    res.data$training$auc_roc_multiclass[[method]] <- optimizeSVGFile(tmp_path)
+                    res.data$training$auc_roc_multiclass_png[[method]] <- convertSVGtoPNG(tmp_path)
                 }
             }
         }
--- server/plots/summary/functions/plots.R
+++ server/plots/summary/functions/plots.R
@@ -1,3 +1,121 @@
+plot_auc_roc_multiclass_testing <- function(roc_data, auc_value, settings, tmp_hash){ 
+
+    theme_set(eval(parse(text=paste0(settings$theme, "()"))))
+   
+	plotData <- ggplot(roc_data, aes(x = FPR, y = TPR, color = Class, group = Class)) +
+			    geom_line() +
+			    coord_equal() +
+			    style_roc(major.breaks = c(0, 0.1, 0.25, 0.5, 0.75, 0.9, 1),
+			              minor.breaks = c(seq(0, 0.1, by = 0.01), seq(0.9, 1, by = 0.01)),
+			              guide = TRUE, 
+			              xlab = "False Positive Rate (FPR) (1 - Specificity)",
+			              ylab = "True Positive Rate (TPR) (Sensitivity)",
+			              theme = theme_bw) + 
+			    geom_abline(slope = 1, intercept = 0, color = "#D3D3D3", alpha = 0.85, linetype="longdash") +
+			    theme(text=element_text(size=settings$fontSize))  + 
+			    scale_fill_brewer(palette=settings$colorPalette) +
+			    labs(title = "Multi-class ROC Curves",
+			         subtitle = paste("Comparison of model performance across different classes | AUC:", round(auc_value, 2)),
+			         color = "Class Comparison") 
+
+
+    tmp_path <- tempfile(pattern = tmp_hash, tmpdir = tempdir(), fileext = ".svg")
+    svg(tmp_path, width = settings$plot_size * settings$aspect_ratio, height = settings$plot_size, pointsize = 12, onefile = TRUE, family = "Arial", bg = "white", antialias = "default")
+        print(plotData)
+    dev.off()  
+
+    return(tmp_path) 
+}
+plot_auc_roc_multiclass_testing_single <- function(roc_data, auc_labels, settings, tmp_hash){ 
+
+    theme_set(eval(parse(text=paste0(settings$theme, "()"))))
+   
+	plotData <- ggplot(roc_data, aes(x = FPR, y = TPR, color = Class)) +
+			    geom_line() + 
+			    coord_equal() +
+			    style_roc(major.breaks = c(0, 0.1, 0.25, 0.5, 0.75, 0.9, 1),
+			              minor.breaks = c(seq(0, 0.1, by = 0.01), seq(0.9, 1, by = 0.01)),
+			              guide = TRUE, 
+			              xlab = "False Positive Rate (FPR) (1 - Specificity)",
+			              ylab = "True Positive Rate (TPR) (Sensitivity)",
+			              theme = theme_bw) + 
+			    geom_abline(slope = 1, intercept = 0, color = "#D3D3D3", alpha = 0.85, linetype="longdash") +
+			    scale_color_brewer(type = "qual", palette = "Set1", labels = auc_labels) +
+			    theme(text=element_text(size=settings$fontSize))  + 
+			    scale_fill_brewer(palette=settings$colorPalette) +
+			    labs(title = "Multi-class ROC Curves (One-vs-All)",
+			         subtitle = paste("Comparison of model performance across different classes"),
+			         color = "Class Comparison") 
+
+
+    tmp_path <- tempfile(pattern = tmp_hash, tmpdir = tempdir(), fileext = ".svg")
+    svg(tmp_path, width = settings$plot_size * settings$aspect_ratio, height = settings$plot_size, pointsize = 12, onefile = TRUE, family = "Arial", bg = "white", antialias = "default")
+        print(plotData)
+    dev.off()  
+
+    return(tmp_path) 
+}
+
+plot_auc_roc_multiclass_training <- function(roc_data, auc_value, settings, tmp_hash){ 
+
+    theme_set(eval(parse(text=paste0(settings$theme, "()"))))
+   
+	plotData <- ggplot(roc_data, aes(x = FPR, y = TPR, color = Label, group = Class)) +
+			    geom_line() +
+			    coord_equal() +
+			    style_roc(major.breaks = c(0, 0.1, 0.25, 0.5, 0.75, 0.9, 1),
+			              minor.breaks = c(seq(0, 0.1, by = 0.01), seq(0.9, 1, by = 0.01)),
+			              guide = TRUE, 
+			              xlab = "False Positive Rate (FPR) (1 - Specificity)",
+			              ylab = "True Positive Rate (TPR) (Sensitivity)",
+			              theme = theme_bw) + 
+			    geom_abline(slope = 1, intercept = 0, color = "#D3D3D3", alpha = 0.85, linetype="longdash") +
+			    theme(text=element_text(size=settings$fontSize))  + 
+			    scale_fill_brewer(palette=settings$colorPalette) +
+			    labs(title = "Multi-class ROC Curves",
+			         subtitle = paste("Comparison of model performance across different classes"),
+			         color = "Class Comparison (AUC)") 
+
+
+    tmp_path <- tempfile(pattern = tmp_hash, tmpdir = tempdir(), fileext = ".svg")
+    svg(tmp_path, width = settings$plot_size * settings$aspect_ratio, height = settings$plot_size, pointsize = 12, onefile = TRUE, family = "Arial", bg = "white", antialias = "default")
+        print(plotData)
+    dev.off()  
+
+    return(tmp_path) 
+}
+
+plot_auc_roc_multiclass_training_single <- function(roc_data, auc_labels, settings, tmp_hash){
+    theme_set(eval(parse(text=paste0(settings$theme, "()"))))
+
+	plotData <- ggplot(roc_data, aes(x = FPR, y = TPR, color = Class)) +
+			    geom_line() + 
+			    coord_equal() +
+			    style_roc(major.breaks = c(0, 0.1, 0.25, 0.5, 0.75, 0.9, 1),
+			              minor.breaks = c(seq(0, 0.1, by = 0.01), seq(0.9, 1, by = 0.01)),
+			              guide = TRUE, 
+			              xlab = "False Positive Rate (FPR) (1 - Specificity)",
+			              ylab = "True Positive Rate (TPR) (Sensitivity)",
+			              theme = theme_bw) + 
+			    geom_abline(slope = 1, intercept = 0, color = "#D3D3D3", alpha = 0.85, linetype="longdash") +
+			    scale_color_brewer(type = "qual", palette = "Set1", labels = auc_labels) +
+			    theme(text=element_text(size=settings$fontSize))  + 
+			    scale_fill_brewer(palette=settings$colorPalette) +
+			    labs(title = "Multi-class ROC Curves (One-vs-All)",
+			         subtitle = paste("Comparison of model performance across different classes"),
+			         color = "Class Comparison") 
+
+    tmp_path <- tempfile(pattern = tmp_hash, tmpdir = tempdir(), fileext = ".svg")
+    svg(tmp_path, width = settings$plot_size * settings$aspect_ratio, height = settings$plot_size, pointsize = 12, onefile = TRUE, family = "Arial", bg = "white", antialias = "default")
+        print(plotData)
+    dev.off()  
+
+    return(tmp_path) 
+}
+
+
+
+### TWO CLASS PLOTS
 plot_auc_roc_training <- function(trainingPredictions, settings, tmp_hash){
 
     theme_set(eval(parse(text=paste0(settings$theme, "()"))))
@@ -26,55 +144,6 @@ plot_auc_roc_training <- function(trainingPredictions, settings, tmp_hash){
     return(tmp_path) 
 }
 
-plot_auc_roc_testing_multiclass <- function(roc_data, auc_value, settings, tmp_hash){
-
-    theme_set(eval(parse(text=paste0(settings$theme, "()"))))
-   
-	plotData <- ggplot(roc_data, aes(x = FPR, y = TPR, color = Class, group = Class)) +
-			    geom_line() +
-			    coord_equal() +
-			    style_roc(major.breaks = c(0, 0.1, 0.25, 0.5, 0.75, 0.9, 1),
-			              minor.breaks = c(seq(0, 0.1, by = 0.01), seq(0.9, 1, by = 0.01)),
-			              guide = TRUE, 
-			              xlab = "False Positive Rate (1 - Specificity)",
-			              ylab = "True Positive Rate (Sensitivity)",
-			              theme = theme_bw) + 
-			    geom_abline(slope = 1, intercept = 0, color = "#D3D3D3", alpha = 0.85, linetype="longdash") +
-			    theme(text=element_text(size=settings$fontSize))  + 
-			    scale_fill_brewer(palette=settings$colorPalette) +
-			    labs(title = "Multi-class ROC Curves",
-			         subtitle = paste("Comparison of model performance across different classes | AUC:", round(auc_value, 2)),
-			         color = "Class Comparison") 
-
-
-    tmp_path <- tempfile(pattern = tmp_hash, tmpdir = tempdir(), fileext = ".svg")
-    svg(tmp_path, width = settings$plot_size * settings$aspect_ratio, height = settings$plot_size, pointsize = 12, onefile = TRUE, family = "Arial", bg = "white", antialias = "default")
-        print(plotData)
-    dev.off()  
-
-    return(tmp_path) 
-}
-
-
-plot_auc_roc_trainig_multiclass <- function(roc_data, auc_labels, settings, tmp_hash){
-    theme_set(eval(parse(text=paste0(settings$theme, "()"))))
-
-	plotData <- ggplot(roc_data, aes(x = FPR, y = TPR, color = Class)) +
-			    geom_line() + 
-			    geom_abline(linetype = "dashed") +
-			    scale_color_brewer(type = "qual", palette = "Set1", labels = auc_labels) +
-			    labs(title = "Training ROC Curves", x = "False Positive Rate (FPR)", y = "True Positive Rate (TPR)") +
-			    theme_minimal() +
-			    theme(legend.title = element_blank()) +
-			    guides(color = guide_legend(title = "Class (AUC)"))
-
-    tmp_path <- tempfile(pattern = tmp_hash, tmpdir = tempdir(), fileext = ".svg")
-    svg(tmp_path, width = settings$plot_size * settings$aspect_ratio, height = settings$plot_size, pointsize = 12, onefile = TRUE, family = "Arial", bg = "white", antialias = "default")
-        print(plotData)
-    dev.off()  
-
-    return(tmp_path) 
-}
 
 plot_auc_roc_testing <- function(testingPredictions, settings, tmp_hash){
     theme_set(eval(parse(text=paste0(settings$theme, "()"))))
@@ -102,24 +171,6 @@ plot_auc_roc_testing <- function(testingPredictions, settings, tmp_hash){
 }
 
 
-plot_auc_roc_testing_full <- function(modelData, settings, tmp_hash){
-    theme_set(eval(parse(text=paste0(settings$theme, "()"))))
-
-	# unique_pred_levels <- unique(c(levels(modelPredictionData$pred), levels(modelPredictionData$obs)))
-
-	referenceData <- modelData$info$data$testing[[modelData$info$outcome]]
-	predictionObject <- modelData$predictions$raw$predictions
-
-	plotData <- mplot_full(tag=referenceData, score=predictionObject[, "B"])
-
-
-    tmp_path <- tempfile(pattern =  tmp_hash, tmpdir = tempdir(), fileext = ".svg")
-    svg(tmp_path, width = settings$plot_size * settings$aspect_ratio, height = settings$plot_size, pointsize = 12, onefile = TRUE, family = "Arial", bg = "white", antialias = "default")
-        print(plotData)
-    dev.off()  
-
-    return(tmp_path) 
-}
 # https://bgreenwell.github.io/pdp/articles/pdp.html
 plot_partial_dependence <- function(modelData, settings, tmp_hash){
     theme_set(eval(parse(text=paste0(settings$theme, "()"))))
