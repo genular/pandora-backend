2329089cff9eabe3ff1e1aed43d00d9c04dac38a
LogIN- <info@ivantomic.com>
2024-04-16 17:14:17 +0100

    [hotfix] multiclass

--- cron/maintenance.sh
+++ cron/maintenance.sh
@@ -8,8 +8,8 @@ if [ -f "$LOG_FILE" ]; then
     log_size=$(wc -c < "$LOG_FILE")  # Get the current file size in bytes
 
     if [ "$log_size" -gt "$MAX_SIZE" ]; then
-        echo "Truncating $LOG_FILE"
         cp /dev/null "$LOG_FILE"
+        echo "===> MAINTENANCE $(date) - Truncating $LOG_FILE because it exceeded $MAX_SIZE bytes" >> "$LOG_FILE"
     fi
 fi
 
@@ -18,30 +18,39 @@ APP_DIR_FRONTEND="/var/www/genular/pandora"
 
 # Remove hs_err files from backend directory if it exists
 if [ -d "$APP_DIR_BACKEND" ]; then
-    rm "$APP_DIR_BACKEND"/hs_err_pid*.log
+    find "$APP_DIR_BACKEND" -name 'hs_err_pid*.log' -exec rm {} \;
+    echo "===> MAINTENANCE $(date) - Removed Java error logs from backend directory" >> "$LOG_FILE"
 fi
+
 
 # Update git repositories if update.txt exists
 UPDATE_FILE="$APP_DIR_BACKEND/server/backend/public/assets/update.txt"
 if [ -f "$UPDATE_FILE" ]; then
-    echo "Update required, updating git repositories..."
+    echo "===> MAINTENANCE $(date) - Update required, updating git repositories..." >> "$LOG_FILE"
+
+    source $UPDATE_FILE
 
     # Update Frontend repository
-    cd "$APP_DIR_FRONTEND" \
-        && git checkout . && git fetch && git checkout master && git pull origin master \
-        && yarn install --check-files \
-        && yarn run webpack:web:prod \
-        --isDemoServer=false \
-        --server_frontend=http://localhost:3010 \
-        --server_backend=http://localhost:3011 \
-        --server_homepage=http://localhost:3010
+    if cd "$APP_DIR_FRONTEND"; then
+        git checkout . && git fetch && git checkout master && git pull origin master && \
+        yarn install --check-files && \
+        yarn run webpack:web:prod --isDemoServer=false --server_frontend=$FRONTEND_URL --server_backend=$BACKEND_URL --server_homepage=$FRONTEND_URL
+        echo "===> MAINTENANCE $(date) - Updated Frontend repository successfully." >> "$LOG_FILE"
+    else
+        echo "===> MAINTENANCE $(date) - Failed to change directory to $APP_DIR_FRONTEND" >> "$LOG_FILE"
+    fi
 
     # Update Backend repository
-    cd "$APP_DIR_BACKEND/server/backend" \
-        && git checkout . && git fetch && git checkout master && git pull origin master \
-        && /usr/bin/php8.2 /usr/local/bin/composer install --ignore-platform-reqs \
-        && /usr/bin/php8.2 /usr/local/bin/composer post-install /tmp/configuration.json
+    if cd "$APP_DIR_BACKEND/server/backend"; then
+        git checkout . && git fetch && git checkout master && git pull origin master && \
+        /usr/bin/php8.2 /usr/local/bin/composer install --ignore-platform-reqs && \
+        /usr/bin/php8.2 /usr/local/bin/composer post-install /tmp/configuration.json
+        echo "===> MAINTENANCE $(date) - Updated Backend repository successfully." >> "$LOG_FILE"
+    else
+        echo "===> MAINTENANCE $(date) - Failed to change directory to $APP_DIR_BACKEND/server/backend" >> "$LOG_FILE"
+    fi
     
     # Delete the update.txt file after updates
     rm "$UPDATE_FILE"
+    echo "===> MAINTENANCE $(date) - Deleted the update.txt file." >> "$LOG_FILE"
 fi
